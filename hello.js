b87205fe51b31632c8d56bb67ce9dea0bfb33fdd
yingxin326 <singingxy@gmail.com>
2025-08-28 16:07:53 +0800

    Delete hello.js

--- hello.js
+++
@@ -1,6 +0,0 @@
-// api/hello.js
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  if (req.method === 'OPTIONS') return res.status(204).end();
-  res.json({ ok: true, time: new Date().toISOString() });
-};
